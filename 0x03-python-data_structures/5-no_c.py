c761092fc1b6c86ada34419ccfcb6e42658bbbcb
eshihanda <eshihandamonn@gmail.com>
2023-11-06 12:27:39 -0800

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    string_1 = ""
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            string_1 += i
-
-        return string_1
+    string_1 = my_string.translate({ord(i): None for i in 'cC'})
+    return string_1
