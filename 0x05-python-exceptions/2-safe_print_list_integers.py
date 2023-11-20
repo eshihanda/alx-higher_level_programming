ca6de13b41d74e53ffeee5518a4522a01d5c1e8b
eshihanda <eshihandamonn@gmail.com>
2023-11-20 11:34:53 -0800

    raise exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            value = my_list[i]
+            if type(value) is int:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+        except ValueError as e:
+            print(str(e))
+        except IndexError as e:
+            raise
+        except TypeError as e:
+            print(str(e))
+            break
+    print()
+    return (count)
+
