7af50b85bb3d6342dc0c32311fcaac1f1eafe693
eshihanda <eshihandamonn@gmail.com>
2023-11-20 05:14:07 -0800

    print an integer value

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,8 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             number += 1
     except IndexError:
-        None
-    print()
-    return number
+        pass
+    finally:
+        print()
+        return number
 
