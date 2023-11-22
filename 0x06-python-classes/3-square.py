c820dd6aa8482efd65d46478a8e0d0d9bfb7f141
eshihanda <eshihandamonn@gmail.com>
2023-11-22 12:44:08 -0800

    area of the square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,16 +4,16 @@ private instance attribute size and public instance method"""
 
 
 class Square:
-    """defines class and instantiates validated private instance attribute
-and also includes public instance method."""
+    """defines class with instantiated and validated private instance attribute
+and public instance method."""
 
     def __init__(self, size=0):
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        """calculates and returns current area of the square"""
+        """calculates and returns current square area"""
         return(self.__size * self.__size)
