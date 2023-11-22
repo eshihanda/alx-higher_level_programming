c13cf75973be1241701388aa8cc1644b3d28e8fb
eshihanda <eshihandamonn@gmail.com>
2023-11-22 13:34:07 -0800

    print the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""creates class Square with
+private instance attribute size and public instance method"""
+
+
+class Square:
+    """defines class with instantiated and validated private instance attribute
+and public instance method."""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """property to retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter to set size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculates and returns current square area"""
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        """prints square of size self.__size using #"""
+        if self.__size > 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
