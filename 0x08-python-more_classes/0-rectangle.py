7368156334fab4866d3eb38e0acb6cf6076f4bb5
eshihanda <eshihandamonn@gmail.com>
2023-11-27 12:05:45 -0800

    create an empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Creates an empty class Rectangle."""
+"""creates empty class Rectangle to define a rectangle"""
+
 
 class Rectangle:
-    """Defines an empty class Rectangle."""
+    """defines an empty class to define a rectangle"""
+
     pass
