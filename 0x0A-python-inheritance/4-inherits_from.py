791f7cc15969729b15c1c3dc9e7836e33da5f2fa
eshihanda <eshihandamonn@gmail.com>
2024-01-11 01:49:36 -0800

    is a subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """returns true if an object is an instance of a subclass"""
-    if issubclass(type(obj), a_class) and type(obj) !=a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
