367a6852f9718e4c8c43eeaf29fb790ba61ea672
eshihanda <eshihandamonn@gmail.com>
2024-01-11 03:33:13 -0800

    try testing against

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""defines a class rectangle that inherits
-from the basegeometry class"""
+"""defines class Rectangle that inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """class that inherits from base class"""
+    """class for rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
-        """initialization"""
-        super().integer_validator("height", height)
-        super().integer_validator("width", width)
-        self.__height = height
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
