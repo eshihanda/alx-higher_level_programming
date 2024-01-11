5f8b62d6f01a583f5b38fe2e1fccf547bf52c34f
eshihanda <eshihandamonn@gmail.com>
2024-01-11 03:25:34 -0800

    try testing against

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,11 +5,12 @@ from the basegeometry class"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """class that inherits from base class"""
     def __init__(self, width, height):
-        """initializes"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
+        """initialization"""
+        super().integer_validator("height", height)
+        super().integer_validator("width", width)
         self.__height = height
+        self.__width = width
