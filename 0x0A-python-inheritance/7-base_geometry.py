163cc6e28e609ac94c86b9e44b02229684d868bd
eshihanda <eshihandamonn@gmail.com>
2024-01-11 03:09:23 -0800

    try testing against

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""defines a class with public instance methods for are and integer validation"""
+"""defines a class with public instance methods
+for are and integer validation"""
 
 
 class BaseGeometry:
     """class with public instance methods"""
     def area(self):
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         self.name = name
         self.value = value
