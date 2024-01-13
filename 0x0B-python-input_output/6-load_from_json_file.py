38d03f7a5866a719b487e2b76dd2b7f4c3c108dd
eshihanda <eshihandamonn@gmail.com>
2024-01-13 04:51:44 -0800

    loads a json file to a py object

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines a function that creates an object from a json file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an object from a json file"""
+    with open(filename) as f:
+        return json.load(f)
