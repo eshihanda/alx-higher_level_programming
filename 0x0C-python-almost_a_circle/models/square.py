ae061c07e051f9b97b8da50b90e02855b41fe10f
eshihanda <eshihandamonn@gmail.com>
2024-01-17 11:16:02 -0800

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
-"""defines a class square that inherits from rectangle"""
+"""defines Square class that inherits from Rectangle"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """the square class"""
+    """class for Square instances with
+    attributes from Rectangle class"""
     def __init__(self, size, x=0, y=0, id=None):
-        """initialization"""
+        """initializes Square instance"""
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """str representation"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        self.size = size
 
     @property
     def size(self):
-        return self.width
+        """gets private instance attribute size"""
+        return (self.width)
 
     @size.setter
     def size(self, value):
-        self.width = value
-        self.height = value
+        """sets private instance attribute size and
+        uses to set width and height"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+            self.height = value
+
+    def __str__(self):
+        """override __str__ with new string in the format
+        [Square] (<id>) <x>/<y> - <size>"""
+        str_rep = "[Square] ({}) {}/{} - {}".format(
+            str(self.id), str(self.x), str(self.y), str(self.width))
+        return (str_rep)
