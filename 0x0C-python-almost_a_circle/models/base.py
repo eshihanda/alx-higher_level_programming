2608dc02be7273957850feaace9bf9815ce53567
eshihanda <eshihandamonn@gmail.com>
2024-01-18 05:01:36 -0800

    dict to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 all other classes in this project"""
 
 
+import json
+
+
 class Base():
     """the base class"""
 
@@ -15,3 +18,10 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return the json string representation"""
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,8 +3,9 @@
 
 
 import unittest
-
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -24,3 +25,18 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b3.id, 12)
         b4 = Base()
         self.assertEqual(b4.id, 3)
+
+    def test_to_json_string(self):
+        """ 15 check static method to_json_string BASE """
+        b1 = Base()
+        b2 = Base()
+        lis = [b1.__dict__, b2.__dict__]
+        dic = {"id": 1}
+        b_json_dict_str = Base.to_json_string([])
+        self.assertEqual(b_json_dict_str, "[]")
+        b_json_dict_str = Base.to_json_string(None)
+        self.assertEqual(b_json_dict_str, "[]")
+        b_json_dict_str = Base.to_json_string(lis)
+        self.assertNotEqual(type(b_json_dict_str), type(dic))
+        self.assertEqual(type(b_json_dict_str), str)
+        self.assertEqual(b_json_dict_str, '[{"id": 1}, {"id": 2}]')
