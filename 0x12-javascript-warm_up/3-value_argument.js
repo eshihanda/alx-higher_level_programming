5f92c8cf3746e0549a6acbc0229ead4142114468
eshihanda <eshihandamonn@gmail.com>
2024-03-12 15:53:05 +0300

    convert to integer

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-// Prints first argument passed
-const args = process.argv.slice(2);
-if (args[0] === undefined) {
+const arg = process.argv[2];
+if (arg === undefined) {
   console.log('No argument');
 } else {
-  console.log(args[0]);
+  console.log(arg);
 }
