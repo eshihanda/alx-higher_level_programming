4eb91fab1234afd4bf5659ec785a6ef2bb99e153
eshihanda <eshihandamonn@gmail.com>
2024-03-12 17:58:09 +0300

    use loops

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myArgs = process.argv.splice(2);
+const num = Number(myArgs[0]);
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  for (i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
