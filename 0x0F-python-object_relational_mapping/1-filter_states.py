c615adefa6a534dd080ad1a2800eac03fdaae1b8
eshihanda <eshihandamonn@gmail.com>
2024-03-18 05:48:26 -0700

    states starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+lists all states from the database in ascending order
+where name of state starts with 'N' using MySQLdb
+"""
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                        passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states\
+        WHERE name\
+        LIKE BINARY 'N%'\
+        ORDER BY id ASC")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
