90d3f28296f259d5b8fa5cd671ac083043d50721
eshihanda <125441440+eshihanda@users.noreply.github.com>
2024-04-10 09:26:33 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-exports.esrever = function (list) {
-  const listcpy = list.slice();
-  let i = 0;
-  for (i = 0; i < list.length; i++) {
-    list[i] = listcpy[list.length - i - 1];
-  }
-  return list;
-};
--- 0x13-javascript_objects_scopes_closures/README.md
+++
@@ -1,2 +0,0 @@
-0x13-javascript_objects_scopes_closures
-mainly about object, scopes, and closures in java script
