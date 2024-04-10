2fab3ee2442e2adfe2af576d80e1b8eff1623ba3
eshihanda <eshihandamonn@gmail.com>
2024-04-10 20:10:39 +0300

    seven multi

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const myVar = 'C is fun';
 const myArgs = process.argv.splice(2);
 const num = Number(myArgs[0]);
 if (isNaN(num)) {
-	  console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	  let i = 0;
-	  for (i = 0; i < num; i++) {
-		      console.log(myVar);
-		    }
+  let i = 0;
+	 for (i = 0; i < num; i++) {
+		 console.log('C is fun');
+	 }
 }
