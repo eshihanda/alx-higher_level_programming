709c6331243bb29b46b55706234ca3b1489f69b6
eshihanda <eshihandamonn@gmail.com>
2024-04-10 19:55:33 +0300

    run semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const myArg = process.argv.slice(2);
 const num = Number(myArg[0]);
 if (isNaN(num)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number: ' + num);
+  console.log('My number: ' + num);
 }
