fef359fdfa547888a584d673276cbb3206fc0c66
eshihanda <125441440+eshihanda@users.noreply.github.com>
2024-04-10 09:25:54 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const myArg = process.argv.slice(2);
-const num = Number(myArg[0]);
-if (isNaN(num)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + num);
-}
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const myArgs = process.argv.slice(2);
-const num1 = Number(myArgs[0]);
-const num2 = Number(myArgs[1]);
-function add (a, b) {
-  return (a + b);
-}
-console.log(add(num1, num2));
