58a341b409b4f09ecc11be0e5644a14a133ed22d
eshihanda <eshihandamonn@gmail.com>
2024-04-15 03:00:38 -0700

    list states using sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).order_by(State.id).all()
-    for state  in states:
+    for state in session.query(State):
         print("{}: {}".format(state.id, state.name))
         session.close()
