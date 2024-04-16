f661666d05787ceffb90ed572e89b7cd35a021c4
eshihanda <eshihandamonn@gmail.com>
2024-04-16 00:24:44 -0700

    all states using sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,7 +15,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).all()
-    for state in session.query(State):
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         session.close()
