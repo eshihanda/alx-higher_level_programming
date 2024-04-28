7bd3df5d81beb74d97db46c3b7aecf5c326cf018
eshihanda <eshihandamonn@gmail.com>
2024-04-28 14:29:11 -0700

    using json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" post request from url with params in url"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+    data = {'q': q}
+    r = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        if not r.json():
+            print("No result")
+        else:
+            print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
+    except ValueError:
+        print("Not a valid JSON")
