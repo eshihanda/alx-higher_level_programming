bb6d27a0a8c3a4248d28a1025d0a807d03b77061
eshihanda <eshihandamonn@gmail.com>
2024-05-23 23:05:41 +0300

    read file using js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-//read contents of a file
+// read contents of a file
 const fs = require('fs');
 const file = process.argv[2];
-fs.readFile(file, 'utf8', function(err, data) {
-    if (err)
-        return console.log(err);
-    console.log(data);
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) { return console.log(err); }
+  console.log(data);
 });
