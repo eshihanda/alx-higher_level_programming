7f6e879fb233382cb4570c4e5fd4c55fdc75a85c
eshihanda <eshihandamonn@gmail.com>
2024-05-23 23:14:06 +0300

    write to a file using js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// writes a string to a file
+const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
+fs.writeFile(file, string, 'utf8', function (err) {
+  if (err) return console.log(err);
+});
